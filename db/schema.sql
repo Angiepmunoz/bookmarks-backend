4a5278fe480c3e4a15401b357c76112b238855d2
Myra Smith <myra.fowlkes.smith@gmail.com>
2023-05-02 09:12:14 -0400

    adds not null constraints

--- db/schema.sql
+++ db/schema.sql
@@ -6,18 +6,18 @@ CREATE DATABASE bookmarks_dev;
 CREATE TABLE bookmarks (
  id SERIAL PRIMARY KEY,
  name TEXT NOT NULL,
- url TEXT,
+ url TEXT NOT NULL,
  category TEXT,
- is_favorite BOOLEAN
+ is_favorite BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE reviews (
  id SERIAL PRIMARY KEY,
- reviewer TEXT,
- title TEXT,
- content TEXT,
+ reviewer TEXT NOT NULL,
+ title TEXT NOT NULL,
+ content TEXT NOT NULL,
  rating NUMERIC,
  CHECK (rating >= 0 AND rating <= 5),
- bookmark_id INTEGER REFERENCES bookmarks (id)
+ bookmark_id INTEGER NOT NULL REFERENCES bookmarks (id)
  ON DELETE CASCADE
 );
